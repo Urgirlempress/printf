51ef707607eff57245e14e6338886d7d1d29ebe3
chiomaizuokwu <chigirlangel@yahoo.com>
2022-08-09 17:26:26 -0700

    Even more functions

--- functions2.c
+++ functions2.c
@@ -176,4 +176,14 @@ int print_rot13string(va_list types, char buffer[],
 				count++;
 				break;
 			}
+		}
+		if (!in[j])
+		{
+			x = str[i];
+			write(1, &x, 1);
+			count++;
+		}
+	}
+	return (count);
+}
 
