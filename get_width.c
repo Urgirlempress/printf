198b158b9095c249c513f3be3073f47e38a8645d
chiomaizuokwu <chigirlangel@yahoo.com>
2022-08-09 17:44:24 -0700

    get_width

--- get_width.c
+++ get_width.c
@@ -20,4 +20,17 @@ int get_width(const char *format, int *i, va_list list)
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
+	*i = curr_i - 1;
+
+	return (width);
+}
